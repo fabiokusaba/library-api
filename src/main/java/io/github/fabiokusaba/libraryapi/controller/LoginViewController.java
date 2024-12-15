fee1383a6367e7da75aab296a3fd2208c4fd2e3a
fabiokusaba <fabinhokb44@gmail.com>
2024-12-14 21:39:55 -0300

    adicionado página home

--- src/main/java/io/github/fabiokusaba/libraryapi/controller/LoginViewController.java
+++ src/main/java/io/github/fabiokusaba/libraryapi/controller/LoginViewController.java
@@ -1,11 +1,17 @@
 package io.github.fabiokusaba.libraryapi.controller;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 // Agora preciso dizer como ele vai acessar essa página e para isso vamos criar um controller e anotar com Controller
 // Qual a diferença de Controller para RestController? O RestController é para API, para requisições REST onde vou
 // tratar com JSON, vou mandar e receber objetos JSON. E o Controller é quando estou utilizando páginas web
+// Quando utilizamos a anotação Controller ele espera que essa String que estou retornando seja uma página, ou seja, é
+// como se eu estivesse retornango a página 'login.html'
+// Então, como é que eu faço para dizer que eu quero retornar String? Utilizo a anotação ResponseBody, desta forma ele
+// vai pegar o retorno e colocar no corpo da resposta e não vai esperar uma página
 @Controller
 public class LoginViewController {
 
@@ -15,5 +21,11 @@ public class LoginViewController {
     @GetMapping("/login")
     public String paginaLogin() {
         return "login";
+    }
+
+    @GetMapping("/")
+    @ResponseBody
+    public String paginaHome(Authentication authentication) {
+        return "Olá " + authentication.getName();
     }
 }
