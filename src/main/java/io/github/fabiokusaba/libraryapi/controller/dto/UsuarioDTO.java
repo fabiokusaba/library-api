e6bac4d0b46bc480a4876de0c779047d102e20ce
fabiokusaba <fabinhokb44@gmail.com>
2024-12-16 19:59:07 -0300

    adicionado o campo email e validação dos campos

--- src/main/java/io/github/fabiokusaba/libraryapi/controller/dto/UsuarioDTO.java
+++ src/main/java/io/github/fabiokusaba/libraryapi/controller/dto/UsuarioDTO.java
@@ -1,6 +1,20 @@
 package io.github.fabiokusaba.libraryapi.controller.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 import java.util.List;
 
-public record UsuarioDTO(String login, String senha, List<String> roles) {
+public record UsuarioDTO(
+        @NotBlank(message = "Campo obrigatório")
+        String login,
+
+        @Email(message = "Insira um email válido")
+        @NotBlank(message = "Campo obrigatório")
+        String email,
+
+        @NotBlank(message = "Campo obrigatório")
+        String senha,
+
+        List<String> roles) {
 }
