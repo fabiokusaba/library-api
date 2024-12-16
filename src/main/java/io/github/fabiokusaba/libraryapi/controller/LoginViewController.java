271e69ce81e2f6c0f443cdf474ea889716c012f2
fabiokusaba <fabinhokb44@gmail.com>
2024-12-16 19:49:47 -0300

    adicionado comandos sql

--- src/main/java/io/github/fabiokusaba/libraryapi/controller/LoginViewController.java
+++ src/main/java/io/github/fabiokusaba/libraryapi/controller/LoginViewController.java
@@ -1,5 +1,6 @@
 package io.github.fabiokusaba.libraryapi.controller;
 
+import io.github.fabiokusaba.libraryapi.security.CustomAuthentication;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,6 +27,10 @@ public class LoginViewController {
     @GetMapping("/")
     @ResponseBody
     public String paginaHome(Authentication authentication) {
+        if (authentication instanceof CustomAuthentication customAuth) {
+            System.out.println(customAuth.getUsuario());
+        }
+
         return "Olá " + authentication.getName();
     }
 }
