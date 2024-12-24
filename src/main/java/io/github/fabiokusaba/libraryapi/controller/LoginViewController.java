100e4cc5ea44934149cfbf67bb9b0905832343a8
fabiokusaba <fabinhokb44@gmail.com>
2024-12-24 20:45:36 -0300

    adicionado rota para retornar o authorization code

--- src/main/java/io/github/fabiokusaba/libraryapi/controller/LoginViewController.java
+++ src/main/java/io/github/fabiokusaba/libraryapi/controller/LoginViewController.java
@@ -3,6 +3,7 @@ package io.github.fabiokusaba.libraryapi.controller;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 // Agora preciso dizer como ele vai acessar essa página e para isso vamos criar um controller e anotar com Controller
@@ -27,5 +28,11 @@ public class LoginViewController {
     @ResponseBody
     public String paginaHome(Authentication authentication) {
         return "Olá " + authentication.getName();
+    }
+
+    @GetMapping("/authorized")
+    @ResponseBody
+    public String getAuthorizationCode(@RequestParam("code") String code) {
+        return "Seu authorization code: " + code;
     }
 }
