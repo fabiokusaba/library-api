2bb8d747a0320189a4dc6cf308f965d9c4049261
fabiokusaba <fabinhokb44@gmail.com>
2024-12-25 18:10:19 -0300

    configurando o Security Scheme

--- src/main/java/io/github/fabiokusaba/libraryapi/config/OpenApiConfiguration.java
+++ src/main/java/io/github/fabiokusaba/libraryapi/config/OpenApiConfiguration.java
@@ -1,8 +1,12 @@
 package io.github.fabiokusaba.libraryapi.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
 // Customizando a documentação da nossa API através de uma classe de configuração
@@ -16,9 +20,17 @@ import org.springframework.context.annotation.Configuration;
                         email = "fabinhokb44@gmail.com",
                         url = "library-api.com"
                 )
-        )
+        ),
+        security = {
+                @SecurityRequirement(name = "bearerAuth")
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer",
+        in = SecuritySchemeIn.HEADER
 )
 public class OpenApiConfiguration {
-
-
 }
